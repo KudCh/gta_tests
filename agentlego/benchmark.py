43519700a28deea505245cd33757d1f5e2ccd204
Kristina KUDRYAVTSEVA <kristina.kudryavtseva@uni.lu>
2025-09-26 15:35:51 +0200

    tools adaptation

--- agentlego/benchmark.py
+++ agentlego/benchmark.py
@@ -4,6 +4,37 @@ from agentlego.utils import load_or_build_object, require
 from agentlego.tools import BaseTool
 from agentlego.types import ImageIO, Annotated, Info
 
+# from transformers import AutoProcessor, AutoModelForCausalLM
+# import torch
+# import re
+
+# -----------------------------
+# Monkey patch for transformers.AutoModel.from_pretrained
+import transformers
+
+old_from_pretrained = transformers.AutoModel.from_pretrained
+
+def patched_from_pretrained(*args, **kwargs):
+    kwargs.setdefault("attn_implementation", "eager")
+    return old_from_pretrained(*args, **kwargs)
+
+transformers.AutoModel.from_pretrained = patched_from_pretrained
+
+old_causal_from_pretrained = transformers.AutoModelForCausalLM.from_pretrained
+def patched_causal_from_pretrained(*args, **kwargs):
+    kwargs.setdefault("attn_implementation", "eager")
+    return old_causal_from_pretrained(*args, **kwargs)
+
+transformers.AutoModelForCausalLM.from_pretrained = patched_causal_from_pretrained
+
+# Patch LlavaLlamaForCausalLM if available
+if hasattr(transformers, "LlavaLlamaForCausalLM"):
+    old_llava_from_pretrained = transformers.LlavaLlamaForCausalLM.from_pretrained
+    def patched_llava_from_pretrained(*args, **kwargs):
+        kwargs.setdefault("attn_implementation", "eager")
+        return old_llava_from_pretrained(*args, **kwargs)
+    transformers.LlavaLlamaForCausalLM.from_pretrained = patched_llava_from_pretrained
+# -----------------------------
 
 class QwenVLInferencer:
 
@@ -67,40 +98,55 @@ class CountGivenObject(BaseTool):
     default_desc = 'The tool can count the number of a certain object in the image.'
 
     @require('mmpretrain')
-    def __init__(self,
-                 model: str = 'llava-7b-v1.5_vqa',
-                 device: str = 'cpu',
-                 toolmeta=None):
+    def __init__(
+        self,
+        #model: str = 'llava-7b-v1.5_vqa',
+        model: str = 'blip-base_3rdparty_caption',
+        # model_id: str = 'llava-hf/llava-1.5-7b-hf',
+        device: str = 'cpu',
+        toolmeta=None
+    ):
         super().__init__(toolmeta=toolmeta)
         self.model = model
+        #self.model_id = model_id
         self.device = device
 
     def setup(self):
         from mmengine.registry import DefaultScope
         from mmpretrain.apis import VisualQuestionAnsweringInferencer
+
         with DefaultScope.overwrite_default_scope('mmpretrain'):
             self._inferencer = load_or_build_object(
                 VisualQuestionAnsweringInferencer,
                 model=self.model,
-                device=self.device,
+                device=self.device
             )
+        # self.processor = AutoProcessor.from_pretrained(self.model_id, trust_remote_code=True, use_fast=False)
+        # self.model = AutoModelForCausalLM.from_pretrained(
+        #     self.model_id,
+        #     torch_dtype=torch.float16 if self.device == 'cuda' else torch.float32,
+        #     device_map='auto' if self.device == 'cuda' else None,
+        #     trust_remote_code=True, 
+        #     attn_implementation='eager',
+        # )
+        # self.model.to(self.device)
+        # self.model.eval()
 
     def apply(
         self,
         image: ImageIO,
         text: Annotated[str, Info('The object description in English.')],
-        #bbox: Annotated[Optional[str],
-        #                Info('The bbox coordinate in the format of `(x1, y1, x2, y2)`')] = None,
-        bbox: Annotated[str, Info('The bbox coordinate in the format of `(x1, y1, x2, y2)`')],
+        bbox: Annotated[Optional[str],
+                        Info('The bbox coordinate in the format of `(x1, y1, x2, y2)`')] = None,
     ) -> int:
         import re
         if bbox is None:
             image = image.to_array()[:, :, ::-1]
         else:
             from agentlego.utils import parse_multi_float
-            #x1, y1, x2, y2 = (int(item) for item in parse_multi_float(bbox))
-            x1, y1, x2, y2 = map(int, bbox.split(","))
+            x1, y1, x2, y2 = (int(item) for item in parse_multi_float(bbox))
             image = image.to_array()[y1:y2, x1:x2, ::-1]
+
         res = self._inferencer(image, f'How many {text} are in the image? Reply a digit')[0]['pred_answer']
         res = re.findall(r'\d+', res)
         if len(res) > 0:
@@ -108,6 +154,16 @@ class CountGivenObject(BaseTool):
         else:
             return 0
 
+        # #run inference
+        # inputs = self.processor(images=image, text=f'How many {text} are in the image? Reply a digit', return_tensors="pt").to(self.device)
+        # with torch.no_grad():
+        #     outputs = self.model.generate(**inputs, max_new_tokens=16)
+        # res = self.processor.batch_decode(outputs, skip_special_tokens=True)[0]
+        # res = re.findall(r'\d+', res)
+        # if len(res) > 0:
+        #     return int(res[0])
+        # else:
+        #     return 0
 
 class RegionAttributeDescription(BaseTool):
     default_desc = 'Describe the attribute of a region of the input image.'
@@ -129,7 +185,7 @@ class RegionAttributeDescription(BaseTool):
             self._inferencer = load_or_build_object(
                 VisualQuestionAnsweringInferencer,
                 model=self.model,
-                device=self.device,
+                device=self.device
             )
 
     def apply(
